2024406abb941949a1c2e63e92217556c67399ec
Clair <Clair@LAPTOP-85D2A8RG>
2020-10-22 14:10:20 +0200

    finish EditClientController

--- CPOA_THIL_BRUNGARD_Projet/src/application/controller/edit/EditClientController.java
+++ CPOA_THIL_BRUNGARD_Projet/src/application/controller/edit/EditClientController.java
@@ -2,20 +2,24 @@ package application.controller.edit;
 
 import java.net.URL;
 
+
 import java.util.ResourceBundle;
 
 import dao.Persistance;
 import dao.factory.DAOFactory;
+import dao.modele.ClientDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import modele.Client;
 
+
 public class EditClientController implements Initializable {
-	@FXML private Button btnCreer;
+	@FXML private Button btnModif;
 	@FXML private Label lblAffichage;
 	@FXML private TextField editNom;
 	@FXML private TextField editPrenom; 
@@ -25,74 +29,55 @@ public class EditClientController implements Initializable {
 	@FXML private TextField editVille;
 	@FXML private TextField editPays;
 	
+	ClientDAO clientDAO = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE).getClientDAO();
+	private Client selectedItem;
+	
 	@Override
     public void initialize(URL location, ResourceBundle resources) {
-	    try {
-	    	this.editNom.setText("");
-	    	this.editPrenom.setText("");
-	    	this.editRue.setText("");
-	    	this.editNo.setText("");
-	    	this.editCodePostal.setText("");
-	    	this.editVille.setText("");
-	    	this.editPays.setText("");
-		} catch (Exception e) {
-		}
+	}
+	
+	public void initData(Client client) {
+		selectedItem = client;
+		
+		editNom.setText(client.getNom());
+		editPrenom.setText(client.getPrenom());
+		editNo.setText(client.getNumero());
+		editRue.setText(client.getRue());
+		editCodePostal.setText(client.getCodePostal());
+		editVille.setText(client.getVille());
+		editPays.setText(client.getPays());
+		
+		lblAffichage.setText("");
 	}
 	
 	@FXML
-	public void creerClient() {
+	public void modifClient() {
 		String nom = editNom.getText().trim();
-    	String prenom = editPrenom.getText().trim();
-    	String numero = editNo.getText().trim();
-    	String rue = editRue.getText().trim();
-    	String codePostal = editCodePostal.getText().trim();
-    	String ville = editVille.getText().trim();
-    	String pays = editPays.getText().trim();
-		String erreur="";
+		String prenom = editPrenom.getText().trim();
+		String numero = editNo.getText().trim();
+		String rue = editRue.getText().trim();
+		String codePostal = editCodePostal.getText().trim();
+		String ville = editVille.getText().trim();
+		String pays = editPays.getText().trim();
 		
-		
-//		//on convertit le tarif qui est en String en int 
-//		try {
-//			tarif = Float.parseFloat(editTarif.getText().trim());
-//		}
-//		catch (NumberFormatException e) {
-//		}
-//		
-//		if (nom.isEmpty()) {
-//			erreur = erreur + "\nLe nom est vide";
-//		}
-//		if (desc.isEmpty()) {
-//			erreur = erreur + "\nLa description est vide";
-//		}
-//		if (tarif <= 0) {
-//			erreur = erreur + "\nVeuillez saisir un tarif raisonnable";
-//		}
-//		if (selectItem == null) {
-//			erreur = erreur + "\nVeuillez selectionner une categorie";
-//		}
-//		
-//		if (erreur != "") {
-//			this.lblAffichage.setTextFill(Color.web("#bb0b0b"));
-//			this.lblAffichage.setText(erreur);
-//		}
-//		else {
-		
-			//on creer une instance de produit 
-		this.lblAffichage.setTextFill(Color.web("#000000"));
+		try {
+			//On creer dans la DAO l'objet Produit
+			Client client = new Client(selectedItem.getId(), nom, prenom, numero, rue, codePostal, ville, pays);
+			clientDAO.update(client);
 			
-			try {
-				Client c1 = new Client(1, nom, prenom, numero, rue, codePostal, ville, pays);
-				DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE).getClientDAO().create(c1); 
-				this.lblAffichage.setText(c1.toStringUtilisateur()); 
-				System.out.println(c1.toString());
-					
-			} catch (Exception e) {
-				erreur = e.getMessage();
-				this.lblAffichage.setText(erreur);
-				this.lblAffichage.setTextFill(Color.web("#000000"));
-			} 
-			initialize(null, null);
-			this.lblAffichage.setTextFill(Color.web("#000000"));
+			this.selectedItem = client;
 			
+			//On récupère la scene sur laquelle le btnModif est place et on ferme cette fenetre
+			Stage stage = (Stage) btnModif.getScene().getWindow();
+			stage.close();
+		}
+		catch (Exception e) {
+			this.lblAffichage.setTextFill(Color.RED);
+			this.lblAffichage.setText(e.getMessage());
+		}
+	}
+	
+	public Client getSelectedItem() {
+		return selectedItem;
 	}
 }
--- CPOA_THIL_BRUNGARD_Projet/src/application/controller/page/PageClientController.java
+++ CPOA_THIL_BRUNGARD_Projet/src/application/controller/page/PageClientController.java
@@ -1,19 +1,34 @@
 package application.controller.page;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.controller.MainController;
+import application.controller.add.AjoutClientController;
+import application.controller.edit.EditClientController;
 import dao.Persistance;
 import dao.factory.DAOFactory;
 import dao.modele.ClientDAO;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import modele.Client;
+
 
 public class PageClientController implements Initializable {
 
@@ -30,9 +45,55 @@ public class PageClientController implements Initializable {
 	@FXML private Button searchClient;
 	@FXML private Button detailClient;
 	
+	@SuppressWarnings("unused")
 	private MainController main;
+	private Client client; 
 	
 	ClientDAO clientDAO = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE).getClientDAO();
+	
+	
+	//Initialisation des donnees + ajout des listeners
+	public void initData() {
+		this.nomClient.setCellValueFactory(new PropertyValueFactory<>("nom"));
+		this.prenomClient.setCellValueFactory(new PropertyValueFactory<>("prenom"));
+		this.cpClient.setCellValueFactory(new PropertyValueFactory<>("codePostal"));
+		this.villeClient.setCellValueFactory(new PropertyValueFactory<>("ville"));
+		this.paysClient.setCellValueFactory(new PropertyValueFactory<>("pays"));
+		
+		try {
+			this.tabClient.getItems().addAll(clientDAO.findAll());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//Definit si les boutons sont desactives ou non
+		this.tabClient.getSelectionModel().selectedItemProperty().addListener(
+				(observale, odlValue, newValue) -> {
+					this.addClient.setDisable(newValue != null);
+					this.deleteClient.setDisable(newValue == null);
+					this.editClient.setDisable(newValue == null);
+					this.client = tabClient.getSelectionModel().getSelectedItem();
+				});
+		
+		//event qui annule la selection actuelle de la tableView si on selectionne une ligne vide
+		this.tabClient.addEventFilter(MouseEvent.MOUSE_CLICKED, evt -> {
+		    Node source = evt.getPickResult().getIntersectedNode();
+		   
+		    // move up through the node hierarchy until a TableRow or scene root is found 
+		    while (source != null && !(source instanceof TableRow)) {
+		        source = source.getParent();
+		    }
+
+		    // clear selection on click anywhere but on a filled row
+		    if (source == null || (source instanceof TableRow && ((TableRow<?>) source).isEmpty())) {
+		    	tabClient.getSelectionModel().clearSelection();
+		    }
+		});
+		
+		this.deleteClient.setDisable(true);
+		this.editClient.setDisable(true);
+		
+	}
 	
 	public void init(MainController mainController) {
 		main = mainController;
@@ -40,33 +101,90 @@ public class PageClientController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		System.out.println("ok");
-		
-		nomClient.setCellValueFactory(new PropertyValueFactory<>("nom"));
-		prenomClient.setCellValueFactory(new PropertyValueFactory<>("prenom"));
-		cpClient.setCellValueFactory(new PropertyValueFactory<>("codePostal"));
-		villeClient.setCellValueFactory(new PropertyValueFactory<>("ville"));
-		paysClient.setCellValueFactory(new PropertyValueFactory<>("pays"));
-		
+		initData(); 
+	}
+	
+	//Charge la page AJoutProduit et recupere les donnees pour les ajouter dans le tableau
+	public void ajoutClient() {
+		Stage nStage = new Stage();
 		try {
-			tabClient.getItems().addAll(clientDAO.findAll());
-		} catch (Exception e) {
-			// TODO Bloc catch généré automatiquement
+			//On charge l'url de la page AjoutProduit.fxml
+			URL fxmlURL=getClass().getResource("/fxml/add/AjoutClient.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Node root = fxmlLoader.load();
+			
+			//On recupere le controleur de la page ModifCateg.fxml
+			AjoutClientController controller = fxmlLoader.getController();
+			
+			//On affiche la fenetre AjoutProduit
+			Scene scene = new Scene((AnchorPane) root, 600, 350);
+			nStage.setScene(scene);
+			nStage.setResizable(false);
+			nStage.setTitle("Creer un client");
+			nStage.initModality(Modality.APPLICATION_MODAL);
+			nStage.showAndWait();
+			
+			if (controller.getClientAjout() != null)
+				tabClient.getItems().add(controller.getClientAjout());
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		this.tabClient.getSelectionModel().selectedItemProperty().addListener(
-				(observale, odlValue, newValue) -> {
-					this.deleteClient.setDisable(newValue == null);
-					this.editClient.setDisable(newValue == null);
-					this.detailClient.setDisable(newValue == null);
-				});
-		
-		this.deleteClient.setDisable(true);
-		this.editClient.setDisable(true);
-		this.detailClient.setDisable(true);
 	}
 
 	
+	//Charge la page ModifProduit et recupere les donnees pour les modifier dans le tableau
+	public void modifClient() {
+		Stage nStage = new Stage();
+		try {
+			//On charge l'url de la page ModifCateg.fxml
+			URL fxmlURL=getClass().getResource("/fxml/edit/ModifClient.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Node root = fxmlLoader.load();
+			
+			//On recupere le controleur de la page ModifCateg.fxml
+			EditClientController controller = fxmlLoader.getController();
+			
+			//On charge les donnees de la ligne selectionnee dans la classe controleur EditCategorieController
+			controller.initData(tabClient.getSelectionModel().getSelectedItem());
+			
+			//On affiche la fenetre ModifCateg
+			Scene scene = new Scene((AnchorPane) root, 600, 350);
+			nStage.setScene(scene);
+			nStage.setResizable(false);
+			nStage.setTitle("Modifier un client");
+			nStage.initModality(Modality.APPLICATION_MODAL);
+			nStage.showAndWait();
+			
+			//On modifie l'objet dans le tableau
+			tabClient.getItems().set(
+					tabClient.getItems().indexOf(controller.getSelectedItem()), 
+					controller.getSelectedItem());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Supprime la valeur dans le tableau et dans la dao
+	public void supprClient() {
+		//Ouvre une fenetre d'alerte pour confirer la suppresion
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Alerte suppression");
+		alert.setContentText("Etes vous certain de vouloir supprimer ce client ?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK){
+			try {
+				clientDAO.delete(client); 
+				tabClient.getItems().remove(client);
+				tabClient.getSelectionModel().clearSelection();
+			} 
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}	
+		else {
+			tabClient.getSelectionModel().clearSelection();
+		}
+	}
 }
