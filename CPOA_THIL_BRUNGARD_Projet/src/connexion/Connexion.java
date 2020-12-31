566fc04ef29b479ccb226c19f345c2c2b65dc2be
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2020-12-31 16:25:29 +0100

    Update Connexion.java

--- CPOA_THIL_BRUNGARD_Projet/src/connexion/Connexion.java
+++ CPOA_THIL_BRUNGARD_Projet/src/connexion/Connexion.java
@@ -7,42 +7,19 @@ import java.sql.SQLException;
 public class Connexion {
 	
 	public static Connection creeConnexion() {		
-		  //Pour se connecter a un serveur local
-//		  String url = "jdbc:mysql://localhost/brungard3u_pullmoche"; 
-//		  url += "?serverTimezone=Europe/Paris"; 
-//		  String login = "root"; 
-//		  String pwd = "root";
 		
-//		  String url = "jdbc:mysql://localhost/brungard3u_pullmochetest"; 
-//		  url += "?serverTimezone=Europe/Paris"; 
-//		  String login = "root"; 
-//		  String pwd = "root";
-		 
-		 
-//		  String url =
-//		  "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/brungard3u_pullmoche"; url
-//		  += "?serverTimezone=Europe/Paris"; String login = "brungard3u_appli"; String
-//		  pwd = "31906904";
-		  
-//		  String url ="jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/brungard3u_pullmocheTEST"; 
-//		  url += "?serverTimezone=Europe/Paris"; 
-//		  String login = "brungard3u_appli"; 
-//		  String pwd = "31906904";
-		
-		 String url ="jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/thil32u_cpoa"; 
-		 url += "?serverTimezone=Europe/Paris"; 
-		 String login = "thil32u_appli"; 
-		 String pwd = "07112001ClaireT";
-		 
-			 
-		 
+		//Pour se connecter a un serveur local
+		String url = "jdbc:mysql://localhost/dbname"; 
+		String login = "root"; 
+		String pwd = "root";
+	 
 		Connection maConnexion = null;
 		try { 
 			maConnexion = DriverManager.getConnection(url, login, pwd);
-			} 
+		} 
 		catch (SQLException sqle) {
 			System.out.println("Erreur connexion" + sqle.getMessage());
-			}
+		}
 		
 		return maConnexion;
 	}
